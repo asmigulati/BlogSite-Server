b7a492d2a088a6faf7c9534d91fed62c5a5426c2
asmigulati <140332527+asmigulati@users.noreply.github.com>
2023-11-03 17:00:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,23 @@ def cosine_similarity(vec_a, vec_b):
     norm_a = np.linalg.norm(vec_a)
     norm_b = np.linalg.norm(vec_b)
     return dot_product / (norm_a * norm_b)
+data = {
+    'Timestamp': ['2023-02-13 22:48:20', '2023-02-13 22:48:20', '2023-02-13 22:48:10', '2023-02-13 22:48:10', 
+                  '2023-02-13 22:48:10', '2023-02-13 22:33:30', '2023-02-13 22:33:10', '2023-02-13 22:18:10', 
+                  '2023-02-13 22:49:00', '2023-02-13 21:48:50', '2023-02-13 21:48:50', '2023-02-13 21:48:50', 
+                  '2023-02-13 21:48:10', '2023-02-13 21:48:10', '2023-02-13 21:19:10', '2023-02-13 21:18:10', 
+                  '2023-02-13 21:18:10', '2023-02-13 21:03:40', '2023-02-13 21:03:30', '2023-02-13 21:03:30'],
+    'Activity': ['desk is idle', 'bed is idle', 'closet is idle', 'refrigerator is idle', 
+                 'Isabella Rodriguez is stretching', 'shelf is idle', 'desk is neat and organized', 
+                 'Isabella Rodriguez is writing in her journal', 'desk is idle', 
+                 'Isabella Rodriguez is taking a break', 'bed is idle', 
+                 'Isabella Rodriguez is cleaning up the kitchen', 'refrigerator is idle', 'bed is being used', 
+                 'shelf is idle', 'Isabella Rodriguez is watching a movie', 'shelf is organized and tidy', 
+                 'desk is idle', 'Isabella Rodriguez is reading a book', 'bed is idle']
+}
 
-# Sample DataFrame to store embeddings
-df = pd.DataFrame({
-    'Time': ['2023-01-01', '2023-01-02'],
-    'String': ['Hello world', 'Another sentence'],
-    'Embed': [embed('Hello world'), embed('Another sentence')]
-})
+df = pd.DataFrame(data)
+df['Embed'] = df['Activity'].apply(lambda x: embed([x]))
 
 # Streamlit interface
 st.title('Vector Database Query')
